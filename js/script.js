46b4c827f2eba195b9ffc723c88df276d5db4a12
Bynntangg <bintang@gmail.com>
2025-08-17 23:43:51 +0700

    kjn

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
- // Mobile Navigation
+
+// Mobile Navigation
         const hamburger = document.querySelector('.hamburger');
         const navMenu = document.querySelector('.nav-menu');
         
